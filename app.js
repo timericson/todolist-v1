a635c699c29bb00059d8ddcb616e1d58f6ad8994
Tim Ericson <smallitsolutions1@gmail.com>
2020-05-19 10:48:54 +0930

    Create work list,<%-Include-%> Header-Footer as seperate ejs docs.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -37,7 +38,7 @@ app.get("/", function(req, res) {
 
 
   res.render("list", {
-    dayOfTheWeek: day,
+    listTitle: day,
     newListItems: items
   });
 
@@ -46,18 +47,45 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  let item = req.body.newItem;
+//console.log(req.body);
 
-items.push(item);
+let item = req.body.newItem;
 
-
+if (req.body.list === "Work") {
+  workItems.push(item);
+  res.redirect("/work");
+} else {
+  items.push(item);
   res.redirect("/");
+}
+
+
+
+
+// items.push(item);
+//
+//
+//   res.redirect("/");
 
 
 });
 
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems:workItems});
+});
+
+app.post("/work", function(req, res){
 
 
+
+  let item = req.body.newItem;
+ workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
